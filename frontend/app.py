c8e18a68dc5f34e78d749ffe8a1455a4a684f94f
Confidence Affang <coaffa01@wsc.edu>
2025-04-17 00:22:53 -0500

    Started the django backend

--- frontend/app.py
+++ frontend/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def helloWord():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("./auth/login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("./auth/signup.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
