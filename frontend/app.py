d051f93569d09f18aa6226ceb8f538cc79a0ed7f
Confidence Affang <coaffa01@wsc.edu>
2025-04-17 12:02:03 -0500

    almost done with the landing page

--- frontend/app.py
+++ frontend/app.py
@@ -2,9 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+sample_jobs = [
+    {"title": "Software Developer", "image": "software_developer.webp"},
+    {"title": "Doctor", "image": "doctor.avif"},
+    {"title": "Chef", "image": "chef.jpg"},
+    {"title": "Teacher", "image": "teacher.jpg"},
+    {"title": "Aerospace Engineer", "image": "aero.webp"},
+    {"title": "Archeologist", "image": "arch.jpg"},
+    {"title": "Astronomer", "image": "astro.jpg"},
+    {"title": "Chemist", "image": "chem.jpg"},
+    {"title": "zoologist", "image": "zoo.jpg"},
+    {"title": "Writer", "image": "writer.jpg"}
+]
+
 @app.route("/")
 def helloWord():
-    return render_template("index.html")
+    return render_template("index.html", jobs= sample_jobs)
 
 @app.route("/login")
 def login():
